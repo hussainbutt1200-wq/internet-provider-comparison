278e7f8bb58a8842375cc7d661a8233a4ea5a08f
hussainbutt1200-wq <hussain.butt1200@gmail.com>
2026-04-18 13:30:11 +0500

    Fix: Remove incorrect blogs import

--- src/lib/module/home/Home.jsx
+++ src/lib/module/home/Home.jsx
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import UsePurpose from "../components/UsePurpose";
 import Providers from "../components/Providers";
-import Blogs from "../components/blogs";
 import BlogSection from "../components/BlogSection";
 
 const Home = () => {
